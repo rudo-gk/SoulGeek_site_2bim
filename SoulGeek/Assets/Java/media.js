09405f478f8a18e3929d49b15892d913ae7609b3
Eduardo Sanches <edujunior2010outlook.com@gmail.com>
2026-06-26 23:14:59 -0300

    Update media.js

--- SoulGeek/Assets/Java/media.js
+++ SoulGeek/Assets/Java/media.js
@@ -28,18 +28,133 @@ async function buscarSerie() {
   genero.innerHTML = serie.show.genres;
 
   const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
-const interacoesUsuario = document.getElementById("interacoesUsuario");
-if (usuarioLogado === null) {
+  const interacoesUsuario = document.getElementById("interacoesUsuario");
+  if (usuarioLogado === null) {
     interacoesUsuario.style.display = "none";
-}
+  }
+  const id = serieAtual.show.id;
+  if (usuarioLogado.notas && usuarioLogado.notas[id]) {
+    document.getElementById("notaUsuario").value = usuarioLogado.notas[id];
+    document.getElementById("notaExibida").innerHTML =
+      "Sua nota: " + usuarioLogado.notas[id] + "/5";
+  }
+
+  const btnLike = document.getElementById("btnLike");
+
+  let jaCurtiu = false;
+
+  for (let i = 0; i < usuarioLogado.curtidos.length; i++) {
+    if (usuarioLogado.curtidos[i] === id) {
+      jaCurtiu = true;
+      break;
+    }
+  }
+
+  if (jaCurtiu) {
+    btnLike.classList.remove("btn-danger");
+    btnLike.classList.add("btn-success");
+    btnLike.innerHTML = '<i class="bi bi-heart-fill"></i> Curtido';
+  }
+
+  const btnWatchlist = document.getElementById("btnWatchlist");
+
+  let jaAssistiu = false;
+
+  for (let i = 0; i < usuarioLogado.watchlist.length; i++) {
+    if (usuarioLogado.watchlist[i] === id) {
+      jaAssistiu = true;
+      break;
+    }
+  }
+
+  if (jaAssistiu) {
+    btnWatchlist.classList.remove("btn-warning");
+    btnWatchlist.classList.add("btn-success");
+    btnWatchlist.innerHTML = '<i class="bi bi-clock-history"></i> Adicionado';
+  }
 }
 
 const btnSalvarNota = document.getElementById("btnSalvarNota");
 
 btnSalvarNota.addEventListener("click", () => {
-    const nota = document.getElementById("notaUsuario").value;
-    console.log(serieAtual.show.id);
-    console.log(nota);
+  const nota = document.getElementById("notaUsuario").value;
+  const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
+  const id = serieAtual.show.id;
+  usuarioLogado.notas[id] = nota;
+  localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado));
+  const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].email === usuarioLogado.email) {
+      usuarios[i].notas[id] = nota;
+      break;
+    }
+  }
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  location.reload();
+});
+
+const btnLike = document.getElementById("btnLike");
+
+btnLike.addEventListener("click", () => {
+  const id = serieAtual.show.id;
+  const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
+  const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  let jaCurtiu = false;
+
+  for (let i = 0; i < usuarioLogado.curtidos.length; i++) {
+    if (usuarioLogado.curtidos[i] === id) {
+      jaCurtiu = true;
+      usuarioLogado.curtidos.splice(i, 1); // remove
+      location.reload();
+      break;
+    }
+  }
+
+  if (jaCurtiu === false) {
+    usuarioLogado.curtidos.push(id); // adiciona
+  }
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].email === usuarioLogado.email) {
+      usuarios[i].curtidos = usuarioLogado.curtidos;
+      location.reload();
+      break;
+    }
+  }
+  localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado));
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  console.log(usuarioLogado.curtidos);
+});
+
+const btnWatchlist = document.getElementById("btnWatchlist");
+btnWatchlist.addEventListener("click", () => {
+  const id = serieAtual.show.id;
+  const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
+  const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  let jaAssistiu = false;
+
+  for (let i = 0; i < usuarioLogado.watchlist.length; i++) {
+    if (usuarioLogado.watchlist[i] === id) {
+      jaAssistiu = true;
+      usuarioLogado.watchlist.splice(i, 1); // remove
+      location.reload();
+      break;
+    }
+  }
+
+  if (jaAssistiu === false) {
+    usuarioLogado.watchlist.push(id); // adiciona
+  }
+
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].email === usuarioLogado.email) {
+      usuarios[i].watchlist = usuarioLogado.watchlist;
+      location.reload();
+      break;
+    }
+  }
+  localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado));
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  console.log(usuarioLogado.watchlist);
 });
 
 buscarSerie();
